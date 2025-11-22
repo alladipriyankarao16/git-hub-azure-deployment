aa3d6674a4a5d2715e1ad215c5fe3f61a9dbccf4
Abilabs <abilash6891@gmail.com>
2025-11-23 10:21:55 +1100

    workflow111

---
+++ dev-env-terraform/main.tf
@@ -0,0 +1,33 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "=4.1.0"
+    }
+  }
+  backend "azurerm" {
+    resource_group_name  = "rg-sqlmi-abipriya001"
+    storage_account_name = "priyaabiterra"
+    container_name       = "tfstate"
+    key                  = "dev/terraform.tfstate"
+  }
+}
+
+variable "subscription_id" {
+  description = "Azure Subscription ID"
+  type        = string
+}
+
+provider "azurerm" {
+  features {}
+  subscription_id = var.subscription_id
+}
+
+resource "azurerm_resource_group" "example" {
+  name     = "rg-github-dev"
+  location = "australiaeast"
+
+  tags = {
+    Environment = "dev"
+  }
+}
